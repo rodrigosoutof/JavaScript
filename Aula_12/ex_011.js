c8d5da44cc7e26f8010529a9da0db0e530b28f78
rodrigosoutof <rodrigosoutof@gmail.com>
2022-05-02 20:33:42 -0300

    exercícios 12

---
+++ Aula_12/ex_011.js
@@ -0,0 +1,8 @@
+var idade = 68
+if (idade <18){
+    console.log(`Não vota`)
+} else if( idade <18 || idade > 65){
+    console.log(`Voto opcional`)
+} else {
+    console.log(`Voto obrigatorio`)
+}
