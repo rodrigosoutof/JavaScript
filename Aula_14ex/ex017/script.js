47538f47f173d1e1d6385f6b01a90ede504e1888
rodrigosoutof <rodrigosoutof@gmail.com>
2022-05-17 20:55:13 -0300

    inicio exerc 17

--- Aula_14ex/ex017/script.js
+++ Aula_14ex/ex017/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function multiplicar (){
+    let numb = Number( document.getElementById('txttab'))
+    let cont = Number(0)
+    let res  = document.querySelector('div#res')
+
+    while ( cont <= 10){
+        res = numb * cont
+        cont ++
+        res.innerHTML `${numb} X ${cont}`
+    }
+
+}
