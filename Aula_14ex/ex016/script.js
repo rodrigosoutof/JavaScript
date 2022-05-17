274177e1a86dd9a1b2538dc766491d6cad448605
rodrigosoutof <rodrigosoutof@gmail.com>
2022-05-17 20:34:03 -0300

    exercício 16

--- Aula_14ex/ex016/script.js
+++ Aula_14ex/ex016/script.js
@@ -1,18 +1,30 @@
-function(verificar) {
-    var ini = document.getElementsByName('ini')
-    var fim = document.getElementsByClassName('fim')
-    var pas = document.getElementsByClassName('pas')
-    var res = document.querySelector('div#res')
+function contar() {
+    let ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    let res = document.querySelector('div#res')
 
-    do {
-        ini = ini + pas
-        
-    } while (ini <= fim)
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        /* Realizando a conversão das variaveis que estão com string*/
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+    if (p <= 0){
+        window.alert('Passo inválido! Considerando Passo 1')
+        p = 1
+    }
 
-    res.innerHTML = ini 
+    if (i < f) { // contagem crescente
+        for(let cont = i; cont <= f; cont += p)
+        res.innerHTML += `${cont} \u{1f449} `
 
-
-
-
-
+    } else { // contagem regressiva
+        for (let cont = i; cont >= f; cont -=p)
+        res.innerHTML += `${cont} \u{1f449} `
+    }
+    res.innerHTML += `\u{1f3c1}`
+    }
 }
